a83c9f66bba0f5725f54fe7a3c12c746e5ac3198
sinnerr79 <169039799+sinnerr79@users.noreply.github.com>
2024-05-06 14:09:50 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+# Весь код бота находится в main.py
+token = "7026393630:AAEmyuEfkKB9vTTmFmqA0TkMbBfMrv7H7-8" # Токен бота
+botname = "CasinoBot" # Имя бота
+bmoney = 10000 # Сколько будет выдано игроку при старте
+ownerid =  917572018
+starting_balance = 10  # Замените на ваше начальное значение баланса
+
